1a4714c658d9964bac24d6798b29b0d7f67b72f8
Chris Li <chrisandkitty@163.com>
2017-01-17 17:30:17 +0800

    Init Project base on Angular-Cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ClarityModule } from 'clarity-angular';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ClarityModule.forChild(),
     FormsModule,
     HttpModule
   ],
